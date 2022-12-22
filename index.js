9d206d1330ab9a74e8e7a9256f6d435d90b3b894
deniz <denizaskar@gmail.com>
2022-12-22 15:33:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const navbarEl=document.querySelector(".navbar")
+const bottomContainerEl= document.querySelector(".bottom-container")
+window.addEventListener("scroll",()=>{
+    if(window.scrollY>bootomContainerEl.offsetTop- navbarEl.offsetHeight-50){
+        navbarEl.classList.add("active");
+    }else{
+        navbarEl.classList.remove("active");
+    }
+})
